a73469a69c0e2eec287da010d2a4c389da36829e
Kyle Zucker <zkyle12@gmail.com>
2016-12-19 21:00:21 -0700

    library working-ish, have to reconstruct objects on load

--- ideabox.js
+++ ideabox.js
@@ -3,13 +3,54 @@ var body = $(".body")
 var save = $(".save")
 var bottom = $(".bottom-section")
 
-var cardLibrary = {}
+function Library() {}
 // cardLibrary = {cardID: Card}
 // {id1: cardObject, id2: cardobject}
+
+Library.prototype.store = function () {
+  console.log(this)
+  console.log(JSON.stringify(this))
+  console.log('end logg');
+  localStorage.setItem('lib1', [JSON.stringify(this)])
+
+
+  var back = JSON.parse(localStorage.getItem('lib1'))
+  console.log(back);
+
+};
+
+Library.prototype.load = function () {
+  //check if localStorage, if so, load the saved library
+  if(localStorage.length) {
+    var tempCardStore = []
+    var lib = JSON.parse(localStorage.getItem('lib1'))
+    console.log(lib);
+    for (var card in lib) {
+      console.log(lib[card]);
+      tempCardStore.push(lib[card])
+    }
+    tempCardStore.forEach(function(card) {
+      console.log(card);
+    })
+  }
+};
+
+cardLibrary = new Library
 
 $("form").submit(function(e) {
    e.preventDefault();
 })
+
+// function Library() {
+//   protos:
+//   -add new card (both on screen and in library)
+//   -remove card
+//   -store library
+//   -load library
+// }
+function pullCardObject(e) {
+  return cardLibrary[$(e).closest('.card').attr('id')]
+}
 
 //new card object > localStorage > card processing > display
 
@@ -50,13 +91,21 @@ Card.prototype.upvoteFunction = function(card) {
   }
 }
 
-
-function pullCardObject(e) {
-  return cardLibrary[$(e).closest('.card').attr('id')]
+Card.prototype.downvoteFunction = function(card) {
+  if (this.quality === 'genius') {
+    this.quality = 'plausible'
+    card.find('.card-quality').replaceWith('<p class = "card-quality">quality: plausible</p>')
+  } else if (this.quality === 'plausible') {
+    this.quality = 'swill'
+    card.find('.card-quality').replaceWith('<p class = "card-quality">quality: swill</p>')
+  }
 }
+
+
 
 bottom.on('click', '.close-card', function() {
   $(this).parent().remove()
+  //Remove from library
 })
 
 bottom.on('click', '.up-arrow', function() {
@@ -64,9 +113,13 @@ bottom.on('click', '.up-arrow', function() {
   thisCard.upvoteFunction($(this).closest('.card'))
 })
 
+bottom.on('click', '.down-arrow', function() {
+  var thisCard = pullCardObject(this)
+  thisCard.downvoteFunction($(this).closest('.card'))
+})
+
+
 save.on("click", function() {
-  console.log(title.val());
-  console.log(body.val());
   var newCard = new Card(title.val(), body.val())
   newCard.addCardToPage()
   cardLibrary[$(newCard).attr('id')] = newCard
