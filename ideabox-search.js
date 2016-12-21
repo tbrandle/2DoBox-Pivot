99841be7dc97c1d9a8380adbd4ba1d8544564497
Kyle Zucker <zkyle12@gmail.com>
2016-12-20 17:15:25 -0700

    finish search

--- ideabox-search.js
+++ ideabox-search.js
@@ -4,18 +4,9 @@
 // var bottom = $(".bottom-section")
 //
 //cardLibrary is our storage object
-console.log('this is connected');
-
-
 var search = $(".search")
 
-function isFunction(functionToCheck) {
-        var getType = {};
-        return functionToCheck && getType.toString.call(functionToCheck) === '[object Function]';
-    }
-
 search.on('keyup', function() {
-  // console.log('search stuff')
   var searchTerm = $(this).val().toLowerCase();
   var cards = []
   for (var cardID in cardLibrary) {
@@ -24,14 +15,12 @@ search.on('keyup', function() {
   onlyCardIDs = cards.filter(function(c) {
     return c > 0 ? c : null
   })
-  console.log(onlyCardIDs)
   onlyCardIDs.map(function (c) {
     var card = cardLibrary[c]
     var title = card.title.toLowerCase()
     var body = card.body.toLowerCase()
     var $card = $('#'+card.id)
     if (title.match(searchTerm) || body.match(searchTerm)) {
-      console.log('match');
       $card.toggle(true)
     } else {
       $card.toggle(false)
