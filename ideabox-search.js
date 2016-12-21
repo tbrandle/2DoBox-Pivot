3261dd495f58ffd92a34f4e9a4ad457a9dbe8063
Kyle Zucker <zkyle12@gmail.com>
2016-12-20 20:24:09 -0700

    refactor search code. Turns out js already does most of what I did.

--- ideabox-search.js
+++ ideabox-search.js
@@ -1,21 +1,13 @@
 //cardLibrary is our storage object
-var search = $(".search")
 
-search.on('keyup', function() {
+$(".search").on('keyup', function() {
   var searchTerm = $(this).val().toLowerCase();
-  var cards = []
-  for (var cardID in cardLibrary) {
-    cards.push(cardID)
-    }
-  onlyCardIDs = cards.filter(function(c) {
-    return c > 0 ? c : null
-  })
-  onlyCardIDs.map(function (c) {
+  var cardIDs = Object.keys(cardLibrary)
+  cardIDs.map(function (c) {
     var card = cardLibrary[c]
-    var title = card.title.toLowerCase()
-    var body = card.body.toLowerCase()
     var $card = $('#'+card.id)
-    if (title.match(searchTerm) || body.match(searchTerm)) {
+    if (card.title.toLowerCase().match(searchTerm)
+    || card.body.toLowerCase().match(searchTerm)) {
       $card.toggle(true)
     } else {
       $card.toggle(false)
