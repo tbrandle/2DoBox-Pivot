f3402b27f90999b8c36b78e430ff88515a099d25
Tim <tbrandle53@gmail.com>
2017-01-24 14:43:58 -0700

    ES6

--- ideabox.js
+++ ideabox.js
@@ -1,69 +1,71 @@
-var $title = $('.title')
-var $body = $('.body')
-var $save = $('.save')
-var $bottom = $('.bottom-section')
+const $title = $('.title');
+const $body = $('.body');
+const $save = $('.save');
+const $bottom = $('.bottom-section');
 
+class Library {
+  store() {
+    localStorage.setItem('lib1', JSON.stringify(this))
+  }
 
-function Library() {}
-
-Library.prototype.store = function () {
-  localStorage.setItem('lib1', JSON.stringify(this))
-}
-
-Library.prototype.load = function (library) {
-  if(localStorage.length) {
-    var libLoad = JSON.parse(localStorage.getItem('lib1'))
-    for (var card in libLoad) {
-      cardLoad = libLoad[card]
-      regenCard = new Card(cardLoad.title, cardLoad.body, cardLoad.quality, cardLoad.id, cardLoad.counter)
-      regenCard.post()
-      library[regenCard.id] = regenCard
+  load(library) {
+    if(localStorage.length) {
+      const libLoad = JSON.parse(localStorage.getItem('lib1'));
+      for (const card in libLoad) {
+        cardLoad = libLoad[card]
+        regenCard = new Card(cardLoad.title, cardLoad.body, cardLoad.quality, cardLoad.id, cardLoad.counter)
+        regenCard.post()
+        library[regenCard.id] = regenCard
+      }
     }
   }
 }
 
-function Card (title, body, quality, id, counter) {
-  this.id = id || Date.now();
-  this.title = title;
-  this.body = body;
-  this.quality = quality || 'swill';
-  this.counter = counter || 0;
-}
+class Card {
+  constructor(title, body, quality, id, counter) {
+    this.id = id || Date.now();
+    this.title = title;
+    this.body = body;
+    this.quality = quality || 'swill';
+    this.counter = counter || 0;
+  }
 
-Card.prototype.post = function () {
-  $bottom.prepend(
-    `<article id = "${this.id}" class = "card">
-       <h2 class = "card-header" contenteditable="true">${this.title}</h2>
-       <button class = "close-card"></button>
-       <p class = "card-body" contenteditable="true">${this.body}</p>
-       <div class = "quality-arrows">
-          <button class = "up-arrow"></button>
-          <button class = "down-arrow"></button>
-          <p class = "card-quality">quality: ${this.quality}</p>
-       </div>
-       <hr>
-    </article>`
-  )
+  post() {
+    $bottom.prepend(
+      `<article id = "${this.id}" class = "card">
+         <h2 class = "card-header" contenteditable="true">${this.title}</h2>
+         <button class = "close-card"></button>
+         <p class = "card-body" contenteditable="true">${this.body}</p>
+         <div class = "quality-arrows">
+            <button class = "up-arrow"></button>
+            <button class = "down-arrow"></button>
+            <p class = "card-quality">quality: ${this.quality}</p>
+         </div>
+         <hr>
+      </article>`
+    )
+
+  }
+
+  voteFunction($card) {
+    if (this.counter <= 0) {
+      this.quality = 'swill';
+      $card.updateQuality(this.quality)
+      this.counter = 0;
+    } else if (this.counter === 1) {
+      this.quality = 'plausible'
+      $card.updateQuality(this.quality)
+    } else if (this.counter >= 2) {
+      this.quality = 'genius';
+      $card.updateQuality(this.quality)
+      this.counter = 2;
+    }
+  }
 }
 
 $.prototype.updateQuality = function (quality) {
   this.find('.card-quality').replaceWith(`<p class = "card-quality">quality: ${quality}</p>`)
 };
-
-Card.prototype.voteFunction = function($card) {
-  if (this.counter <= 0) {
-    this.quality = 'swill';
-    $card.updateQuality(this.quality)
-    this.counter = 0;
-  } else if (this.counter === 1) {
-    this.quality = 'plausible'
-    $card.updateQuality(this.quality)
-  } else if (this.counter >= 2) {
-    this.quality = 'genius';
-    $card.updateQuality(this.quality)
-    this.counter = 2;
-  }
-}
 
 function findCardJq(e) {
   return $(e).closest('.card')
@@ -73,8 +75,8 @@ function thisCard() {
   return cardLibrary[findCardJq(this).attr('id')]
 }
 
-$save.on('click', function() {
-  var newCard = new Card($title.val(), $body.val())
+$save.on('click', () => {
+  const newCard = new Card($title.val(), $body.val());
   newCard.post()
   cardLibrary[$(newCard).attr('id')] = newCard
   cardLibrary.store()
