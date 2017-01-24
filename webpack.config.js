0ae2cf14602782ef21547f6d2cbf4555c2285599
Tim <tbrandle53@gmail.com>
2017-01-24 16:32:40 -0700

    npm working

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+
+const path = require('path');
+
+module.exports = {
+  entry: {
+    main: "./lib/index.js",
+    test: "mocha!./test/index.js"
+  },
+  output: {
+    path: __dirname,
+    filename: "[name].bundle.js"
+  }
+}
