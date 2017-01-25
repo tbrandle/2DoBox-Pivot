ae99a09f4abf1966eb059571c910bf41101eba1b
Tim <tbrandle53@gmail.com>
2017-01-25 13:28:10 -0700

    Selenium is now working

--- test/index.js
+++ test/index.js
@@ -1,7 +1,29 @@
-const assert = require('chai').assert
 
-describe('our test bundle', function () {
-  it('should work', function () {
-    assert(true)
+var assert = require('assert');
+var webdriver = require('selenium-webdriver');
+var test = require('selenium-webdriver/testing');
+
+//include timeout
+
+test.describe('our test bundle', function () {
+  let driver
+
+  test.beforeEach(() => {
+    this.timeout(10000);
+    driver = new webdriver.Builder()
+                                .forBrowser('chrome')
+                                .build();
+    driver.get('http://localhost:8080');
+});
+
+  test.afterEach(() => {
+    driver.quit()
+  })
+
+  test.it('should open the browser', function () {
+    const title = driver.findElement({className: "title" })
+
+    title.sendKeys('hello')
+
     })
   })
