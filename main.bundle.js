1f18d5094b08d269d78be49d5ac13467bcc53ae1
Tim <tbrandle53@gmail.com>
2017-01-26 17:08:35 -0700

    Live search

--- main.bundle.js
+++ main.bundle.js
@@ -51,122 +51,258 @@
 	const $save = $('.save');
 	const $cardSection = $('.bottom-section');
 
+	const cardQualities = ['none', 'low', 'normal', 'high', 'critical']
+
+
+	function loadLocalCards() {
+	  const loadedCardArray = JSON.parse(localStorage.getItem('lib1'))
+	  if(loadedCardArray) {
+	    loadedCardArray.forEach((card) => {
+	      console.log(card);
+	      $cardSection.prepend(buildCard(card))
+	    })
+	  }
+	}
+
+	loadLocalCards()
+
+
+	function storeCurrentCardInfo() {
+	  let cardArray = []
+	  cards = $cardSection.children('.card')
+	  cards.each((i) => {
+	    card = $(cards[i]);
+	    const title = card.children('h2').text()
+	    const body = card.children('.card-body').text()
+	    const qualityIndex = card.find('.card-quality').attr('id')
+	    cardArray.unshift({title: title, body: body, qualityIndex: qualityIndex})
+	  })
+	  localStorage.setItem('lib1', JSON.stringify(cardArray))
+	}
+
+
+	function createNewCardBase (title, body) {
+	  const $card = $(
+	    `<article class="card">
+	    <h2 contenteditable="true">${title}</h2>
+	    <p class="card-body" contenteditable="true">${body}</p>
+	    </article>`
+	  )
+	  return $card
+	}
+
+	function buildCard(options) {
+	  const card = createNewCardBase(options.title, options.body)
+	  addDeleteButton(card)
+	  // let cardQualityVar =  cardQualities[qualityIndex]
+	  const arrowBox = $(`<div class="quality-arrows"><p class="card-quality" id=${options.qualityIndex || 2}>quality: ${cardQualities[options.qualityIndex || 2]}</p><div>`)
+	  addDownVoteButton(arrowBox)
+	  addUpVoteButton(arrowBox)
+	  card.append(arrowBox)
+	  return card
+	}
+
+	function addDeleteButton(card) {
+	  const deleteBtn = createButton('close-card');
+	  card.append(deleteBtn);
+	  deleteBtn.on('click',function(){
+	    card.remove();
+	    storeCurrentCardInfo()
+	  });
+	}
+
+	function addUpVoteButton(arrowBox) {
+	  const btn = createButton('up-arrow');
+	  arrowBox.prepend(btn);
+	  btn.on('click',function(){
+	    upVote(arrowBox);
+	  });
+	}
+
+	function addDownVoteButton(arrowBox) {
+	  const btn = createButton('down-arrow');
+	  arrowBox.prepend(btn);
+	  btn.on('click',function(){
+	    downVote(arrowBox);
+	  });
+	}
+
+
+	function upVote(arrowBox) {
+	  console.log('up vote');
+	  const cardQualitySection = arrowBox.children('.card-quality')
+	  let index = cardQualitySection.attr('id')
+	  if (index < cardQualities.length -1) {
+	    index++
+	  }
+	  cardQualitySection.attr('id', index)
+	  cardQualitySection.text(`quality: ${cardQualities[index]}`)
+	  storeCurrentCardInfo()
+	}
+
+	function downVote(arrowBox) {
+	  console.log('down vote');
+	  let index = arrowBox.children('.card-quality').attr('id');
+	  if (index > 0) {
+	    index --;
+	  }
+	  arrowBox.children('.card-quality').attr('id', index);
+	  arrowBox.children('.card-quality').text('quality: ' + cardQualities[index])
+	  storeCurrentCardInfo()
+	}
+
+
+	function createButton(classes) {
+	  return $('<button class="'+classes+'"></button>');
+	}
+
 	//redo all this shit with closures
-
 	$('form').submit(e => {
+	    console.log('saving new card');
 	   e.preventDefault()
+	   const cardInput = {title: $title.val(), body: $body.val()}
+	   $cardSection.prepend(buildCard(cardInput))
+	   storeCurrentCardInfo()
+	   //update and save storage
 	})
 
-	function findCardJq(e) {
-	  return $(e).closest('.card')
-	}
-
-	function thisCard() {
-	  return cardLibrary[findCardJq(this).attr('id')]
-	}
-
-	class Library {
-	  store() {
-	    localStorage.setItem('lib1', JSON.stringify(this))
-	  }
-
-	  load() {
-	    if(localStorage.getItem('lib1')) {
-	      const libLoad = JSON.parse(localStorage.getItem('lib1'))
-	      Object.keys(libLoad).forEach(id => {
-	        const cardLoad = libLoad[id]
-	        const regenCard = new Card(cardLoad.title, cardLoad.body, cardLoad.quality, cardLoad.id, cardLoad.counter)
-	      })
-	    }
-	  }
-	}
-
-	class Card {
-	  constructor(title, body, quality, id, counter) {
-	    this.id = id || Date.now();
-	    this.title = title;
-	    this.body = body;
-	    this.quality = quality || 'swill';
-	    this.counter = counter || 0;
-	    $cardSection.prepend(
-	      `<article id="${this.id}" class="card">
-	         <h2 class="card-header" contenteditable="true">${this.title}</h2>
-	         <button class="close-card"></button>
-	         <p class="card-body" contenteditable="true">${this.body}</p>
-	         <div class="quality-arrows">
-	            <button class="up-arrow"></button>
-	            <button class="down-arrow"></button>
-	            <p class="card-quality">quality: ${this.quality}</p>
-	         </div>
-	         <hr>
-	      </article>`
-	    )
-	    this.$card = $(`#${this.id}`)
-	    cardLibrary[this.id] = this
-	    cardLibrary.store()
-	  }
-
-	  voteFunction() {
-	    if (this.counter <= 0) {
-	      this.quality = 'swill'
-	      this.counter = 0;
-	    } else if (this.counter === 1) {
-	      this.quality = 'plausible'
-	    } else if (this.counter >= 2) {
-	      this.quality = 'genius'
-	      this.counter = 2
-	    }
-	    this.updateQuality(this.quality)
-	  }
-
-	  updateQuality(quality) {
-	    this.$card.find('.card-quality').replaceWith(`<p class = "card-quality">quality: ${quality}</p>`)
-	  }
-
-	  remove() {
-	    delete cardLibrary[this.id]
-	    this.$card.remove()
-	  }
-	}
-
-	$cardSection.on('click', (e) => {
-	  const $target = $(e.target)
-	  const targetClass = $target[0].attributes.class.nodeValue
-	  const card = cardLibrary[$target.closest('.card').attr('id')]
-	  switch (targetClass) {
-	    case 'up-arrow':
-	      card.counter ++;
-	      card.voteFunction();
-	      break
-	    case 'down-arrow':
-	      card.counter --;
-	      card.voteFunction();
-	      break
-	    case 'close-card':
-	      card.remove()
-	      break
-	    default:
-	  }
-	  cardLibrary.store()
-	})
-
-	$save.on('click', () => {
-	  new Card($title.val(), $body.val());
-	})
-
-	$cardSection.on('blur', '.card-header', function() {
-	  thisCard.call(this).title = $(this).text()
-	  cardLibrary.store()
-	})
-
-	$cardSection.on('blur', '.card-body', function() {
-	  thisCard.call(this).body = $(this).text()
-	  cardLibrary.store()
-	})
-
-
-	const cardLibrary = new Library
-	cardLibrary.load()
+	// function findCardJq(e) {
+	//   return $(e).closest('.card')
+	// }
+	//
+	// function thisCard() {
+	//   return cardLibrary[findCardJq(this).attr('id')]
+	// }
+	//
+	// class Library {
+	//   store() {
+	//     localStorage.setItem('lib1', JSON.stringify(this))
+	//   }
+	//
+	//   load() {
+	//     if(localStorage.getItem('lib1')) {
+	//       const libLoad = JSON.parse(localStorage.getItem('lib1'))
+	//       Object.keys(libLoad).forEach(id => {
+	//         const cardLoad = libLoad[id]
+	//         const regenCard = new Card(cardLoad.title, cardLoad.body, cardLoad.quality, cardLoad.id, cardLoad.counter, cardLoad.classList)
+	//       })
+	//     }
+	//   }
+	// }
+	//
+	// class Card {
+	//   constructor(title, body, quality, id, counter, classList) {
+	//     this.id = id || Date.now();
+	//     this.title = title;
+	//     this.body = body;
+	//     this.quality = quality || 'normal';
+	//     this.counter = counter || 2;
+	//     // this.completed = completed || false;
+	//     this.classList = classList || 'card'
+	//
+	//     $cardSection.prepend(
+	//       `<article id="${this.id}" class="${this.classList}">
+	//          <h2 class="card-header" contenteditable="true">${this.title}</h2>
+	//          <button class="close-card"></button>
+	//          <button class="complete-task">complete</button>
+	//          <p class="card-body" contenteditable="true">${this.body}</p>
+	//          <div class="quality-arrows">
+	//             <button class="up-arrow"></button>
+	//             <button class="down-arrow"></button>
+	//             <p class="card-quality">quality: ${this.quality}</p>
+	//          </div>
+	//          <hr>
+	//       </article>`
+	//     )
+	//     this.$card = $(`#${this.id}`)
+	//     cardLibrary[this.id] = this
+	//     cardLibrary.store()
+	//   }
+	//
+	//   voteFunction() {
+	//     if (this.counter <= 0) {
+	//       this.quality = 'none'
+	//       this.counter = 0;
+	//     } else if (this.counter === 1) {
+	//       this.quality = 'low'
+	//     } else if (this.counter === 2) {
+	//       this.quality = 'normal'
+	//     } else if (this.counter === 3) {
+	//       this.quality = 'high'
+	//     } else if (this.counter >= 4) {
+	//       this.quality = 'critical'
+	//       this.counter = 4
+	//     }
+	//     this.$card.find('.card-quality').replaceWith(`<p class = "card-quality">quality: ${this.quality}</p>`)
+	//   }
+	//
+	//   remove() {
+	//     delete cardLibrary[this.id]
+	//     this.$card.remove()
+	//   }
+	//
+	//   complete() {
+	//     this.$card.toggleClass('completed')
+	//     this.classList = this.$card.attr('class')
+	//     // this.completed = !this.completed
+	//     cardLibrary.store()
+	//   }
+	// }
+	//
+	// $cardSection.on('click', (e) => {
+	//   const $target = $(e.target)
+	//   const targetClass = $target[0].attributes.class.nodeValue
+	//   const card = cardLibrary[$target.closest('.card').attr('id')]
+	//   switch (targetClass) {
+	//     case 'up-arrow':
+	//       card.counter ++;
+	//       card.voteFunction();
+	//       break
+	//     case 'down-arrow':
+	//       card.counter --;
+	//       card.voteFunction();
+	//       break
+	// $save.on('click', () => {
+	//   new Card($title.val(), $body.val());
+	// })
+	//     case 'close-card':
+	//       card.remove();
+	//       break
+	//     case 'complete-task':
+	//       card.complete();
+	//       break
+	//   }
+	//   cardLibrary.store()
+	// })
+	//
+	//
+	// $cardSection.on('blur', '.card-header', function() {
+	//   thisCard.call(this).title = $(this).text()
+	//   cardLibrary.store()
+	// })
+	//
+	// $cardSection.on('blur', '.card-body', function() {
+	//   thisCard.call(this).body = $(this).text()
+	//   cardLibrary.store()
+	// })
+	//
+	// $('.search').on('keyup', function() {
+	//   const searchTerm = $(this).val().toLowerCase();
+	//   const cardIDs = Object.keys(cardLibrary)
+	//   cardIDs.forEach( c => {
+	//     const card = cardLibrary[c]
+	//     const $card = $('#'+card.id)
+	//     if (card.title.toLowerCase().match(searchTerm)
+	//     || card.body.toLowerCase().match(searchTerm)) {
+	//       $card.toggle(true)
+	//     } else {
+	//       $card.toggle(false)
+	//     }
+	//   })
+	// })
+	//
+	// const cardLibrary = new Library
+	// cardLibrary.load()
 
 
 /***/ },
